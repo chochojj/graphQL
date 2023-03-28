84df91cc44566e39b0c45cc9196c3e5fbb6b40c0
chochojj <csgy114@gmail.com>
2023-03-28 14:38:20 +0900

    작동 완

--- graphql/src/App.js
+++ graphql/src/App.js
@@ -1,33 +1,82 @@
 import './App.css';
 import { graphql } from "@octokit/graphql";
+import { useState, useEffect } from "react";
 
-async function App() {
-  const token = process.env.REACT_APP_Token;
+const Token = process.env.REACT_APP_Token
+
+// console.log(Token)
+
+const getRepository = async () => {
   const { repository } = await graphql(
-    `
-      {
-        repository(owner: "octokit", name: "graphql.js") {
-          issues(last: 3) {
-            edges {
-              node {
-                title
+    `{
+      repository(name: "agora-states-fe", owner: "codestates-seb") {
+        discussions(first: 10) {
+          edges {
+            node {
+              id
+              title
+              createdAt
+              url
+              author {
+                login
+                avatarUrl
+              }
+              category {
+                name
+              }
+              answer {
+                author {
+                  login
+                }
               }
             }
           }
         }
       }
-    `,
+    }`,
     {
       headers: {
-        authorization: `token ${token}`,
+        authorization: `token ${Token}`,
       },
     }
   );
+  return repository;
+};
 
+function App() {
+  const [agoraData, setAgoraData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getRepository()
+      .then((res) => {
+        setAgoraData(res.discussions.edges);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(Error, error));
+  }, []);
 
   return (
     <div className="App">
-      
+       {isLoading ? (
+        <div>로딩중임</div>
+      ) : (
+        <ul>
+          {agoraData.map((edge) => {
+            return (
+              <li key={edge.node.id}>
+                <img
+                  src={edge.node.author.avatarUrl}
+                  alt={`avatar of ${edge.node.author.login}`}
+                />
+                <div>{`[${edge.node.category.name}]`}</div>
+                <a href={edge.node.url}>{edge.node.title}</a>
+                <p>{edge.node.answer ? "☑" : "☒"}</p>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
